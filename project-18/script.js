717d66abd02af72cb66717530a8d1afb0b19ad00
Melike Sena Erden <melikesenaerden@gmail.com>
2023-12-06 17:00:50 +0300

    finalised project-18

---
+++ project-18/script.js
@@ -0,0 +1,43 @@
+const body=document.body
+const slides=document.querySelectorAll('.slide')
+const leftArrow=document.getElementById('left')
+const rightArrow=document.getElementById('right')
+
+let slideCount = 0
+
+rightArrow.addEventListener('click',()=>{
+    slideCount++
+
+    if(slideCount > slides.length - 1){
+        slideCount=0
+    }
+    setBgToBody()
+    setActiveSlide()
+})
+
+leftArrow.addEventListener('click',()=>{
+    slideCount--
+
+    if(slideCount < 0){
+        slideCount=slides.length-1
+    }
+
+    setBgToBody()
+    setActiveSlide()
+})
+
+setBgToBody()
+
+function setBgToBody() {
+    body.style.backgroundImage=slides[slideCount].style.backgroundImage
+}
+
+
+function setActiveSlide() {
+    slides.forEach((slide)=> slide.classList.remove('active'))
+
+    slides[slideCount].classList.add('active')
+}
+
+
+
