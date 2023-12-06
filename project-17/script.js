e1730a8a06ea5987137e8138f9d8311405c02d2f
Melike Sena Erden <melikesenaerden@gmail.com>
2023-12-06 13:33:41 +0300

    add projecr-17

---
+++ project-17/script.js
@@ -0,0 +1,3 @@
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=c053a9bd28108c4c9010b35dcaeb44e7&page=1'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=c053a9bd28108c4c9010b35dcaeb44e7&query="'
