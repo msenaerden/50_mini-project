03668ee431c09e698709875871541bc24423e30b
Melike Sena Erden <melikesenaerden@gmail.com>
2023-12-18 16:16:55 +0300

    finalised

--- project-30/script.js
+++ project-30/script.js
@@ -7,30 +7,6 @@ const symbolsEl = document.getElementById("symbols");
 const generateEl = document.getElementById("generate");
 const clipboardEl = document.getElementById("clipboard");
 
-console.log(lengthEl);
-
-generateEl.addEventListener("click", () => {
-  const length = +lengthEl.value; //+ işareti koyunca stringi numbera çevivrir
-  const hasLower = lowercaseEl.checked; //checkbox'ların tik olup olmamasına göre true false değeri döner
-  const hasUpper = uppercaseEl.checked;
-  const hasNumber = numbersEl.checked;
-  const hasSymbol = symbolsEl.checked;
-
-  resultEl.innerHTML = generatePassword(
-    length,
-    hasLower,
-    hasUpper,
-    hasNumber,
-    hasSymbol
-  );
-});
-
-function generatePassword(lower, upper, number, symbol, length) {
-  let generatedPassword = "";
-  const typesCount = lower + upper + number + symbol;
-  console.log(typesCount);
-}
-
 const randomFunc = {
   lower: getRandomLower,
   upper: getRandomUpper,
@@ -38,25 +14,67 @@ const randomFunc = {
   symbol: getRandomSymbol,
 };
 
+clipboardEl.addEventListener("click", () => {
+  const password = resultEl.innerText;
+  if (!password) {
+    return;
+  }
+  navigator.clipboard.writeText(password);
+  alert("Password copied to clipboard!");
+});
+
+generateEl.addEventListener("click", () => {
+  const length = +lengthEl.value;
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumber = numbersEl.checked;
+  const hasSymbol = symbolsEl.checked;
+
+  resultEl.innerText = generatePassword(
+    hasLower,
+    hasUpper,
+    hasNumber,
+    hasSymbol,
+    length
+  );
+});
+
+function generatePassword(lower, upper, number, symbol, length) {
+  let generatedPassword = "";
+  const typesCount = lower + upper + number + symbol;
+  const typesArr = [{ lower }, { upper }, { number }, { symbol }].filter(
+    (item) => Object.values(item)[0]
+  );
+
+  if (typesCount === 0) {
+    return "";
+  }
+
+  for (let i = 0; i < length; i += typesCount) {
+    typesArr.forEach((type) => {
+      const funcName = Object.keys(type)[0];
+      generatedPassword += randomFunc[funcName]();
+    });
+  }
+
+  const finalPassword = generatedPassword.slice(0, length);
+
+  return finalPassword;
+}
+
 function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97); //String.fromCharCode her bir string değerinin numerik bir karşılığını oluşturur örneğin a 97'dir. Yani 97'den başlayıp 1 artarak z'ye kadar gider
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 
 function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65); //String.fromCharCode her bir string değerinin numerik bir karşılığını oluşturur örneğin büyük A 65'dir. Yani 97'den başlayıp 1 artarak z'ye kadar gider
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
 function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48); //String.fromCharCode her bir string değerinin numerik bir karşılığını oluşturur örneğin 0 48'dir.
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
 
 function getRandomSymbol() {
-  const symbol = "!^+%&/()=?*,.";
-  return symbol[Math.floor(Math.random() * symbol.length)];
+  const symbols = "!@#$%^&*(){}[]=<>/,.";
+  return symbols[Math.floor(Math.random() * symbols.length)];
 }
-
-console.log(getRandomSymbol());
-
-console.log(getRandomLower());
-
-console.log(randomFunc);
