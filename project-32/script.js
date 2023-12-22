b06c2faf76e9176c6144d3d3c9ac9858fa1bfaaa
Melike Sena Erden <melikesenaerden@gmail.com>
2023-12-22 21:14:14 +0300

    add new project

--- project-32/script.js
+++ project-32/script.js
@@ -1,26 +1,21 @@
 const addBtn = document.getElementById("add");
 
-const notes = JSON.parse(localStorage.getItem("notes"));
-
-if (notes) {
-  notes.forEach((note) => addNewNote(note));
-}
-
-addBtn.addEventListener("click", () => addNewNote());
+addBtn.addEventListener("click", () => addNewNote("type in..."));
 
 function addNewNote(text = "") {
   const note = document.createElement("div");
   note.classList.add("note");
 
   note.innerHTML = `
-    <div class="tools">
+
+  <div class="tools">
         <button class="edit"><i class="fas fa-edit"></i></button>
         <button class="delete"><i class="fas fa-trash-alt"></i></button>
     </div>
 
     <div class="main ${text ? "" : "hidden"}"></div>
     <textarea class="${text ? "hidden" : ""}"></textarea>
-    `;
+  `;
 
   const editBtn = note.querySelector(".edit");
   const deleteBtn = note.querySelector(".delete");
@@ -32,8 +27,6 @@ function addNewNote(text = "") {
 
   deleteBtn.addEventListener("click", () => {
     note.remove();
-
-    updateLS();
   });
 
   editBtn.addEventListener("click", () => {
@@ -42,22 +35,18 @@ function addNewNote(text = "") {
   });
 
   textArea.addEventListener("input", (e) => {
-    const { value } = e.target;
+    const { value } = e.target; //key object value araştır
 
     main.innerHTML = marked(value);
-
-    updateLS();
   });
 
   document.body.appendChild(note);
 }
 
 function updateLS() {
-  const notesText = document.querySelectorAll("textarea");
+  const nodesText = document.querySelectorAll("textarea");
 
   const notes = [];
 
-  notesText.forEach((note) => notes.push(note.value));
-
-  localStorage.setItem("notes", JSON.stringify(notes));
+  nodesText.forEach((note) => notes.push(note.value));
 }
