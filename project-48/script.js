c6fd692d1fcee7af0b75a0847c49b350dbef7ee9
Melike Sena Erden <melikesenaerden@gmail.com>
2024-01-02 17:47:22 +0300

    modified js

---
+++ project-48/script.js
@@ -0,0 +1,28 @@
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const todosUL = document.getElementById("todos");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  addTodo();
+});
+
+function addTodo(todo) {
+  let todoText = input.value;
+
+  if (todo) {
+    todoText = todo.text;
+  }
+
+  if (todoText) {
+    const todoEl = document.createElement("li");
+    if (todo && todo.completed) {
+      todoEl.classList.add("completed");
+    }
+    todoEl.innerText = todoText;
+
+    todosUL.appendChild(todoEl);
+    input.value = "";
+  }
+}
